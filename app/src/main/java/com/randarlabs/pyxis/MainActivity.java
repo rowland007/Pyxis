082ce880521bc1223f25de35770b1e86f9782528
Randy Rowland <randy.rowland@disroot.org>
2018-07-07 03:11:18 -0400

    Add play-pause method
    The method was created to allow the song to either be paused or start
    playing from a paused state.  It also changes the ImageButton to the
    opposite state from what it currently is in.

--- app/src/main/java/com/randarlabs/pyxis/MainActivity.java
+++ app/src/main/java/com/randarlabs/pyxis/MainActivity.java
@@ -2,12 +2,35 @@ package com.randarlabs.pyxis;
 
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.view.View;
+import android.widget.ImageButton;
 
 public class MainActivity extends AppCompatActivity {
+
+    boolean isPaused = false;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+    }
+
+    /*
+    *Plays or pauses the song depending on the current state. The image button is switched to the
+    * opposite state. For example, if the song is currently playing, then the image button is set to
+    * pause and if the song is currently paused, then the image button is set to play.
+    * */
+    @SuppressWarnings("all")
+    void pausePlaySong(View view) {
+        ImageButton pausePlay = (ImageButton) findViewById(R.id.playPauseImageButton);
+        if (isPaused) {
+            //TODO Play the song
+            pausePlay.setImageResource(android.R.drawable.ic_media_pause);
+            isPaused = false;
+        } else {
+            //TODO Pause the song
+            pausePlay.setImageResource(android.R.drawable.ic_media_play);
+            isPaused = true;
+        }
     }
 }
