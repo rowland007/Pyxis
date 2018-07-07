e1e86dfadf36dc82b1bacd22711b6dea7ca51102
Randy Rowland <randy.rowland@disroot.org>
2018-07-07 05:41:13 -0400

    Add methods to main activity

--- app/src/main/java/com/randarlabs/pyxis/MainActivity.java
+++ app/src/main/java/com/randarlabs/pyxis/MainActivity.java
@@ -20,16 +20,17 @@ import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
 import android.view.View;
 import android.widget.ImageButton;
+import android.widget.Toast;
 
 public class MainActivity extends AppCompatActivity {
 
     /*
-     * The status of the current song
+     * The status of the current song.
      * */
     private boolean isPaused = false;
 
     /*
-     * The method used when the application launches
+     * The method used when the application launches.
      * */
     @Override
     protected void onCreate(Bundle savedInstanceState) {
@@ -38,17 +39,47 @@ public class MainActivity extends AppCompatActivity {
     }
 
     /*
+     * The method used to launch the settings activity.
+     * */
+    @SuppressWarnings("all")
+    protected void openMenu(View view) {
+        //TODO slides out menu & remove toast
+        String toastText = "Settings";
+        Toast.makeText(getApplicationContext(), toastText, Toast.LENGTH_SHORT).show();
+    }
+
+    /*
+     * Makes current song a favorite and lets Pandora Music know.
+     * */
+    @SuppressWarnings("all")
+    protected void thumbsUpCurrentSong(View view) {
+        //TODO Make current song a favorite & remove toast
+        String toastText = "Thumbs Up!";
+        Toast.makeText(getApplicationContext(), toastText, Toast.LENGTH_SHORT).show();
+    }
+
+    /*
      * Checks the current status of the player to allow the status to be changed to the opposite
      * status.
      * */
     @SuppressWarnings("all")
-    void pausePlaySong(View view) {
+    protected void pausePlaySong(View view) {
         ImageButton pausePlayButton = (ImageButton) findViewById(R.id.playPauseImageButton);
         isPaused = isPaused ? playSong(pausePlayButton) : pauseSong(pausePlayButton);
     }
 
     /*
-     * Plays the currently loaded song and sets an ImageButton to the paused button
+     * Skips the current playing song and starts playing the next song in the queue.
+     * */
+    @SuppressWarnings("all")
+    protected void nextSong(View view) {
+        //TODO Play the next song & remove toast
+        String toastText = "Skip song";
+        Toast.makeText(getApplicationContext(), toastText, Toast.LENGTH_SHORT).show();
+    }
+
+    /*
+     * Plays the currently loaded song and sets an ImageButton to the paused button.
      *
      * @param playButton the resource id of the button that will be changed
      * @return           if the song is paused
@@ -60,7 +91,7 @@ public class MainActivity extends AppCompatActivity {
     }
 
     /*
-     * Pauses the currently loaded song and sets an ImageButton to the play button
+     * Pauses the currently loaded song and sets an ImageButton to the play button.
      *
      * @param playButton the resource id of the button that will be changed
      * @return           if the song is paused
