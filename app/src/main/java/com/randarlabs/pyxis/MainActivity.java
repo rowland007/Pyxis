3a91063a58fae4b34e8d7f9cb0e0270634d6fbc3
Randy Rowland <randy.rowland@disroot.org>
2018-07-07 05:14:15 -0400

    Chore: Add comments for readability

--- app/src/main/java/com/randarlabs/pyxis/MainActivity.java
+++ app/src/main/java/com/randarlabs/pyxis/MainActivity.java
@@ -1,3 +1,19 @@
+/*
+ * Copyright (C) 2018
+ * @author Randy Rowland <randy.rowland@disroot.org>
+ * This program is free software: you can redistribute it and/or modify it
+ * under the terms of the GNU General Public License version 3, as published
+ * by the Free Software Foundation.
+ *
+ * This program is distributed in the hope that it will be useful, but
+ * WITHOUT ANY WARRANTY; without even the implied warranties of
+ * MERCHANTABILITY, SATISFACTORY QUALITY, or FITNESS FOR A PARTICULAR
+ * PURPOSE.  See the GNU General Public License for more details.
+ *
+ * You should have received a copy of the GNU General Public License along
+ * with this program.  If not, see <http://www.gnu.org/licenses/>.
+ * */
+
 package com.randarlabs.pyxis;
 
 import android.support.v7.app.AppCompatActivity;
@@ -7,8 +23,14 @@ import android.widget.ImageButton;
 
 public class MainActivity extends AppCompatActivity {
 
+    /*
+     * The status of the current song
+     * */
     private boolean isPaused = false;
 
+    /*
+     * The method used when the application launches
+     * */
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
