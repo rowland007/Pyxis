975986d71d48fcbace4d175518b0768bef5248b8
Randy Rowland <randy.rowland@disroot.org>
2018-07-07 04:27:02 -0400

    Refactor: Separate play and pause methods
    The play pause method will check current status of the player and then
    call the opposite method of the current status. For example if the
    current status is paused, the method will call the play method. The same
    is true for the player if the current status is playing, it will pause.
    
    The reason the methods were seperate is to allow the methods to be
    called by other methods.  For example, when the application loads, it
    could call the play method to start a song and ensure the ImageButton is
    set to the correct image.

--- app/src/main/java/com/randarlabs/pyxis/MainActivity.java
+++ app/src/main/java/com/randarlabs/pyxis/MainActivity.java
@@ -7,7 +7,7 @@ import android.widget.ImageButton;
 
 public class MainActivity extends AppCompatActivity {
 
-    boolean isPaused = false;
+    private boolean isPaused = false;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
@@ -16,21 +16,36 @@ public class MainActivity extends AppCompatActivity {
     }
 
     /*
-    *Plays or pauses the song depending on the current state. The image button is switched to the
-    * opposite state. For example, if the song is currently playing, then the image button is set to
-    * pause and if the song is currently paused, then the image button is set to play.
-    * */
+     * Checks the current status of the player to allow the status to be changed to the opposite
+     * status.
+     * */
     @SuppressWarnings("all")
     void pausePlaySong(View view) {
-        ImageButton pausePlay = (ImageButton) findViewById(R.id.playPauseImageButton);
-        if (isPaused) {
-            //TODO Play the song
-            pausePlay.setImageResource(android.R.drawable.ic_media_pause);
-            isPaused = false;
-        } else {
-            //TODO Pause the song
-            pausePlay.setImageResource(android.R.drawable.ic_media_play);
-            isPaused = true;
-        }
+        ImageButton pausePlayButton = (ImageButton) findViewById(R.id.playPauseImageButton);
+        isPaused = isPaused ? playSong(pausePlayButton) : pauseSong(pausePlayButton);
+    }
+
+    /*
+     * Plays the currently loaded song and sets an ImageButton to the paused button
+     *
+     * @param playButton the resource id of the button that will be changed
+     * @return           if the song is paused
+     * */
+    private boolean playSong(ImageButton playButton) {
+        //TODO Play the song
+        playButton.setImageResource(android.R.drawable.ic_media_pause);
+        return false;
+    }
+
+    /*
+     * Pauses the currently loaded song and sets an ImageButton to the play button
+     *
+     * @param playButton the resource id of the button that will be changed
+     * @return           if the song is paused
+     * */
+    private boolean pauseSong(ImageButton pauseButton) {
+        //TODO Pause the song
+        pauseButton.setImageResource(android.R.drawable.ic_media_play);
+        return true;
     }
 }
